b52f9243beac1bf2ed5efbcefc2b8bb7aded32bd
yijinsheng <jinsheng.yi@mi-me.com>
2017-05-03 20:53:18 +0800

    mybatis整合

--- src/main/java/com/example/controller/IndexController.java
+++ src/main/java/com/example/controller/IndexController.java
@@ -1,6 +1,9 @@
 package com.example.controller;
 
+import com.example.mapper.TestMapper;
+import com.sun.istack.internal.Interned;
 import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -12,15 +15,13 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class IndexController {
     private static final Logger log = Logger.getLogger(IndexController.class);
+    @Autowired
+    private TestMapper testMapper;
 
     @RequestMapping("/index")
     public ModelAndView login() {
-
         log.info("asdfasdf");
+        log.info(testMapper.selectByPrimaryKey(1) + "hello");
         return new ModelAndView("index");
-    }
-
-    public static void main(String[] args) {
-        System.out.println(111);
     }
 }
