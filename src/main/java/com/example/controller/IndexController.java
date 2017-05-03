d45b72e03a0fa584cf331095cfe9f82c91e62885
yijinsheng <jinsheng.yi@mi-me.com>
2017-05-03 17:05:05 +0800

    log4j配置

--- src/main/java/com/example/controller/IndexController.java
+++ src/main/java/com/example/controller/IndexController.java
@@ -1,8 +1,8 @@
 package com.example.controller;
 
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 /**
@@ -11,8 +11,16 @@ import org.springframework.web.servlet.ModelAndView;
 @RequestMapping("/test")
 @Controller
 public class IndexController {
+    private static final Logger log = Logger.getLogger(IndexController.class);
+
     @RequestMapping("/index")
-    public ModelAndView login(){
+    public ModelAndView login() {
+
+        log.info("asdfasdf");
         return new ModelAndView("index");
+    }
+
+    public static void main(String[] args) {
+        System.out.println(111);
     }
 }
